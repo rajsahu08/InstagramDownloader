35eb2053f5479783b773cfe65c264cc927a5d7a9
rajsahu08 <rajsahu0702@gmail.com>
2024-08-04 22:50:46 +0530

    Added Backup API

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ download.addEventListener('click',()=>{
         shortcode=shortcode+url[i];
     }
     let link = `https://instagram-bulk-scraper-latest.p.rapidapi.com/media_info_from_shortcode/${shortcode}`;
-    console.log(shortcode);
-    console.log(link);
     getVideo(link);
 });
 let getVideo=async (url)=>{
@@ -32,5 +30,31 @@ let getVideo=async (url)=>{
         }
     } catch (error) {
 	    console.error(error);
+        console.log("Calling 2nd API");
+        getVideo2(search.value);
+    }
+}
+let getVideo2=async(link)=>{
+    const url = `https://instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com/get-info-rapidapi?url=${link}`;
+    const options = {
+    	method: 'GET',
+	    headers: {
+		    'x-rapidapi-key': '5bae2b20b3mshfa5923da98afd77p138521jsnce7c2c61fc0e',
+		    'x-rapidapi-host': 'instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com'
+	    }
+    };
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+	    const videoElement = document.getElementById('myVid');
+        let videoSRC=result.download_url;
+        videoElement.src = videoSRC;
+        const msg=document.querySelector('.msg');
+        if(videoSRC!=""){
+        msg.style.display="block";
+        }
+    } catch (error) {
+        console.error(error);
     }
 }
